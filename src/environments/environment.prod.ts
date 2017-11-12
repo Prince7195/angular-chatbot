f247b6568a2a0057db6f681100d1ea3a0cd6132e
Vijay Deepak <vijaydeepak7195@outlook.com>
2017-11-12 18:52:00 +0530

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  dialogflow: {
+    angularBot: '26e082b7432447a6ba1623c6f18c7151'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  dialogflow: {
+    angularBot: '26e082b7432447a6ba1623c6f18c7151'
+  }
 };
